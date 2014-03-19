7f4707775d82dd8ee9529a0f47d35b61dfa5fb5a
Martijn Braam <martijn@brixit.nl>
2014-03-19 17:21:13 +0100

    JsonGenerator now extracts class attributes

--- src/JsonGenerator.php
+++ src/JsonGenerator.php
@@ -3,40 +3,63 @@ namespace UmlGeneratorPhp;
 
 use PhpParser\Node\Stmt;
 
-class JsonGenerator{
+class JsonGenerator
+{
     private $phpparser;
     private $statements;
 
-    public function __construct($inputcode){
+    public function __construct($inputcode)
+    {
         $this->phpparser = new \PhpParser\Parser(new \PhpParser\Lexer);
         $this->statements = $this->phpparser->parse($inputcode);
     }
 
-    public function getJson(){
+    public function getJson()
+    {
         print_r($this->statements);
         $functiontree = $this->parseLevel($this->statements);
         print_r($functiontree);
         return json_encode($functiontree);
     }
 
-    private function parseLevel($statements){
+    private function parseLevel($statements)
+    {
         $ret = [];
-        foreach($statements as $statement){
-            if($statement instanceof Stmt\Class_){
+        foreach ($statements as $statement) {
+            if ($statement instanceof Stmt\Class_) {
                 $node = [
                     'type' => 'class',
                     'name' => $statement->name,
                     'children' => $this->parseLevel($statement->stmts)
                 ];
                 $ret[] = $node;
-            }elseif($statement instanceof Stmt\Function_ || $statement instanceof Stmt\ClassMethod){
+            } elseif ($statement instanceof Stmt\Property) {
                 $node = [
-                    'type' => $statement instanceof Stmt\Function_ ? 'function' : 'method',
+                    'type' => 'attribute',
+                    'name' => $statement->props[0]->name
+                ];
+                $node['visibility'] = 'public';
+                if ($statement->type & Stmt\Class_::MODIFIER_PUBLIC) {
+                    $node['visibility'] = 'public';
+                } elseif ($statement->type & Stmt\Class_::MODIFIER_PROTECTED) {
+                    $node['visibility'] = 'protected';
+                } elseif ($statement->type & Stmt\Class_::MODIFIER_PRIVATE) {
+                    $node['visibility'] = 'private';
+                }
+                if ($statement->type & Stmt\Class_::MODIFIER_STATIC) {
+                    $node['scope'] = 'classifier';
+                } else {
+                    $node['scope'] = 'instance';
+                }
+                $ret[] = $node;
+            } elseif ($statement instanceof Stmt\ClassMethod) {
+                $node = [
+                    'type' => 'method',
                     'name' => $statement->name,
                     'scope' => $statement->getType()
                 ];
                 $params = [];
-                foreach($statement->params as $param){
+                foreach ($statement->params as $param) {
                     $params[] = [
                         'name' => $param->name,
                         'type' => $param->type
@@ -44,22 +67,19 @@ class JsonGenerator{
                 }
                 $node['parameters'] = $params;
 
-                if($statement instanceof Stmt\ClassMethod){
-                    //TODO: Workaround for issue https://github.com/clemens-tolboom/uml-generator-php/issues/4
+                //TODO: Workaround for issue https://github.com/clemens-tolboom/uml-generator-php/issues/4
+                $node['visibility'] = 'public';
+                if ($statement->type & Stmt\Class_::MODIFIER_PUBLIC) {
                     $node['visibility'] = 'public';
-                    if($statement->type & Stmt\Class_::MODIFIER_PUBLIC){
-                        $node['visibility'] = 'public';
-                    }elseif($statement->type & Stmt\Class_::MODIFIER_PROTECTED){
-                        $node['visibility'] = 'protected';
-                    }elseif($statement->type & Stmt\Class_::MODIFIER_PRIVATE){
-                        $node['visibility'] = 'private';
-                    }
-                    if($statement->type & Stmt\Class_::MODIFIER_STATIC){
-                        $node['scope'] = 'classifier';
-                    }else{
-                        $node['scope'] = 'instance';
-                    }
-
+                } elseif ($statement->type & Stmt\Class_::MODIFIER_PROTECTED) {
+                    $node['visibility'] = 'protected';
+                } elseif ($statement->type & Stmt\Class_::MODIFIER_PRIVATE) {
+                    $node['visibility'] = 'private';
+                }
+                if ($statement->type & Stmt\Class_::MODIFIER_STATIC) {
+                    $node['scope'] = 'classifier';
+                } else {
+                    $node['scope'] = 'instance';
                 }
 
                 $ret[] = $node;
