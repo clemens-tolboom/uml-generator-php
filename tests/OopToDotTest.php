730b0339e467e0e14ac63b71353a7242ed279307
Clemens Tolboom <clemens@build2be.com>
2014-03-21 14:19:58 +0100

    Added initial dot test.

---
+++ tests/OopToDotTest.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: clemens
+ * Date: 21-03-14
+ * Time: 11:27
+ */
+
+namespace UmlGeneratorPhp;
+
+use UmlGeneratorPhp\OopFilter;
+use UmlGeneratorPhp\OopToDot;
+
+class OopToDotTest extends \PHPUnit_Framework_TestCase
+{
+    private $parser;
+    private $traverser;
+
+    public function setUp()
+    {
+        $this->parser = new \PhpParser\Parser(new \PhpParser\Lexer);
+        $this->traverser = new \PhpParser\NodeTraverser;
+        $this->traverser->addVisitor(new OopFilter);
+    }
+
+    public function testGenerateHTML()
+    {
+        $code = file_get_contents(__DIR__ . '/data/class01.php');
+        $stmts = $this->parser->parse($code);
+        $data = $this->traverser->traverse($stmts);
+        //var_dump($data);
+        $toDot = new OopToDot();
+        $dot = $toDot->getClassDiagram($data);
+
+        var_dump($dot);
+    }
+
+}
+ 
