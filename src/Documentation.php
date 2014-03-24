38f69ab3996b3c690104beae00077271df688397
Clemens Tolboom <clemens@build2be.com>
2014-03-24 15:37:51 +0100

    Change interface into concreet class.

---
+++ src/Documentation.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: clemens
+ * Date: 21-03-14
+ * Time: 16:11
+ */
+
+namespace UmlGeneratorPhp;
+
+/**
+ * Class Documentation
+ *
+ * Implements default values for sub classes.
+ *
+ * @package UmlGeneratorPhp
+ */
+class Documentation
+{
+    function getSiteURL($data)
+    {
+        return "";
+    }
+
+    function getObjectURL($data)
+    {
+        return "";
+    }
+
+
+    function getMethodURL($data, $classdata)
+    {
+        return "";
+    }
+
+
+    function getPropertyURL($data, $classdata)
+    {
+        return "";
+    }
+
+}
